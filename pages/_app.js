3142913bd63684ad0aaf39a93db75b8563cb6b13
Johannes Braun <Braun-Johannes@outlook.de>
2024-05-13 15:43:54 +0200

    Add Spotlight-Page and ArtPieces-Page and Links

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,36 @@
 import GlobalStyle from "../styles";
+import { SWRConfig } from "swr";
+import useSWR from "swr";
+import Layout from "@/components/Layout/Layout";
+
+async function fetcher(...args) {
+  const response = await fetch(...args);
+  return await response.json();
+}
 
 export default function App({ Component, pageProps }) {
+  const url = "https://example-apis.vercel.app/api/art";
+  const { data: pieces, error, isLoading } = useSWR(url, fetcher);
+
+  if (error) {
+    <>Error...</>;
+  }
+
+  if (isLoading) {
+    <>Is Loading...</>;
+  }
+
+  if (!pieces) {
+    return <>Is loading...</>;
+  }
+
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <SWRConfig value={{ fetcher }}>
+        <Component {...pageProps} pieces={pieces} />
+        <Layout />
+      </SWRConfig>
     </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -2,32 +2,12 @@ import useSWR from "swr";
 import ArtPieces from "@/components/ArtPieces/ArtPieces";
 import Spotlight from "@/components/Spotlight/Spotlight";
 
-async function fetcher(...args) {
-  const response = await fetch(...args);
-  return await response.json();
-}
-
-export default function HomePage() {
-  const url = "https://example-apis.vercel.app/api/art";
-  const { data: pieces, error, isLoading } = useSWR(url, fetcher);
-
+export default function SpotLightPage({ pieces }) {
   function getRandomPiece(array) {
     return array[Math.floor(Math.random() * array.length)];
   }
 
-  if (error) {
-    <>Error...</>;
-  }
-  if (isLoading) {
-    <>Is Loading...</>;
-  }
-
-  if (!pieces) {
-    return <>Is loading...</>;
-  }
-
   const randomPiece = getRandomPiece(pieces);
-  console.log(randomPiece);
 
   return (
     <>
@@ -36,7 +16,6 @@ export default function HomePage() {
         artist={randomPiece.artist}
         dimensions={randomPiece.dimensions}
       />
-      <ArtPieces pieces={pieces} />
     </>
   );
 }
