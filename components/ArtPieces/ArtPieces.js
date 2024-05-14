c0b6466ce1287344947a4ce2a8f2577c93e12500
Johannes Braun <Braun-Johannes@outlook.de>
2024-05-14 12:42:51 +0200

    deleted comment

--- components/ArtPieces/ArtPieces.js
+++ components/ArtPieces/ArtPieces.js
@@ -15,34 +15,3 @@ export default function ArtPieces({ pieces, artPiecesInfo, onToggleFavorite }) {
     </div>
   );
 }
-
-// export default function ArtPieces({ pieces, artPiecesInfo, onToggleFavorite }) {
-//   if (!pieces) {
-//     return <>No data available...</>;
-//   }
-
-//   return (
-//     <div>
-//       {pieces.map((piece) => {
-//         return (
-//           <ul key={piece.slug}>
-//             <ArtPiecePreview
-//               pieces={pieces}
-//               title={piece.name}
-//               artist={piece.artist}
-//               dimensions={piece.dimensions}
-//               isFavorite={
-//                 artPiecesInfo?.find((artPiece) => {
-//                   return artPiece.slug === piece.slug;
-//                 })?.isFavorite
-//               }
-//               onToggleFavorite={() => {
-//                 onToggleFavorite(piece.slug);
-//               }}
-//             />
-//           </ul>
-//         );
-//       })}
-//     </div>
-//   );
-// }
