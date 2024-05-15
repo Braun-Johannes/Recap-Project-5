deb93a72904cb43be9166acdcbb8fb4e6a81b8a5
Johannes Braun <Braun-Johannes@outlook.de>
2024-05-15 11:55:43 +0200

    added local-storage

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import { SWRConfig } from "swr";
 import useSWR from "swr";
 import Layout from "@/components/Layout/Layout";
 import { useState } from "react";
+import useLocalStorage from "use-local-storage";
 
 async function fetcher(...args) {
   const response = await fetch(...args);
@@ -13,8 +14,12 @@ export default function App({ Component, pageProps }) {
   const url = "https://example-apis.vercel.app/api/art";
   const { data: pieces, error, isLoading } = useSWR(url, fetcher);
 
-  const [artPiecesInfo, setArtPiecesInfo] = useState([]);
-  console.log(artPiecesInfo);
+  const [artPiecesInfo, setArtPiecesInfo] = useLocalStorage(
+    "artPiecesInfo",
+    []
+  );
+  // pretty sure default is wrong (toggle no longer working)
+
   function handleIsFavorite(slug) {
     const newPiece = artPiecesInfo.find((piece) => {
       return piece.slug === slug;
